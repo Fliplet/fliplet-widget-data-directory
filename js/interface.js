3ae14e1fb9e389b3503ef961d3b15075bb5dec88
Ricardo Seromenho <ricardo@seromenho.com>
2016-09-30 11:37:46 +0100

    Get data sources from organization
    And add ignore type list

--- js/interface.js
+++ js/interface.js
@@ -1,11 +1,21 @@
 var widgetId = Fliplet.Widget.getDefaultId();
 var data = Fliplet.Widget.getData(widgetId) || {};
-
 var dataDirectoryForm;
+var organizationId = Fliplet.Env.get('organizationId');
+var ignoreDataSourceTypes = ['menu'];
 
-Fliplet.DataSources.get()
+Fliplet.DataSources.get({ organizationId: organizationId })
   .then(function (dataSources) {
-    return Promise.all(dataSources.map(function (dataSource) {
+    var filteredDataSources = dataSources.filter(function (dataSource) {
+      for (var i = 0; i < ignoreDataSourceTypes.length; i++) {
+        if (ignoreDataSourceTypes[i] === dataSource.type) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+    return Promise.all(filteredDataSources.map(function (dataSource) {
       return Fliplet.DataSources.connect(dataSource.id)
         .then(function (source) {
           return source.find();

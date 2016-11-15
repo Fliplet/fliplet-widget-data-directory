d17ec57fcef8b9b9dc4fd253b54f0b94224ee24b
Ricardo Seromenho <ricardo@seromenho.com>
2016-11-15 09:47:13 +0000

    Revert "Merge pull request #12 from Fliplet/get-data-online"
    This reverts commit 878753f52861b238f513da33bca05ba900a019c0, reversing
    changes made to e58b27e5ad220fb3105d4450acf44b9875d99fa9.

--- js/build.js
+++ js/build.js
@@ -1,33 +1,8 @@
 var dataDirectory = {};
 $('[data-directory-id]').each(function(){
-  var container = this;
   var id = $(this).data('directory-id');
   var config = Fliplet.Widget.getData(id);
   if (config.source) {
-    if (Fliplet.Navigator.isOnline()) {
-      Fliplet.DataSources.connect(config.source)
-        .then(function (source) {
-          return source.find();
-        })
-        .then(function (rows) {
-          config.rows = rows.map(function (row) {
-            row.data.dataSourceEntryId = row.id;
-            return row.data;
-          });
-
-          Fliplet.Storage.set('data-directory-rows-' + id, config.rows);
-          dataDirectory[id] = new DataDirectory(config, container);
-        });
-    } else {
-      Fliplet.Storage.get('data-directory-rows-' + id)
-        .then(function (rows) {
-          if (rows) {
-            config.rows = rows;
-            dataDirectory[id] = new DataDirectory(config, container);
-          } else {
-            dataDirectory[id] = new DataDirectory(config, container);
-          }
-        });
-    }
+    dataDirectory[id] = new DataDirectory(config, this);
   }
 });

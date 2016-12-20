3aaff11152fc68409b0d1b527e14aabbf66d5bd3
Ricardo Seromenho <ricardo@seromenho.com>
2016-12-20 19:20:35 +0000

    Fix: allow numbers on fields

--- js/build-directory.js
+++ js/build-directory.js
@@ -795,7 +795,7 @@ DataDirectory.prototype.getEntryField = function( entryIndex, fieldIndex, type )
   }
 
   var valueHTML;
-  if ( (typeof value === 'object' && value && value.value && value.value.length) || (typeof value === 'string' && value.length) ) {
+  if ( (typeof value === 'object' && value && value.value && value.value.length) || (typeof value === 'string' && value.length) || (typeof value === 'number')) {
     if (this.config.show_tags && this.config.tags_field === label && fieldType === 'filter') {
       valueHTML = value.value.split(",").map(function (tag) {
         tag = tag.trim();

bd5ec14c0cd7644955c4c79e606a0558ca8eb735
Ricardo Seromenho <ricardo@seromenho.com>
2017-02-02 03:50:44 +0000

    Change logic to get data
    From local DB
    Or from live and store on PV

--- js/build.js
+++ js/build.js
@@ -6,36 +6,61 @@ $('[data-directory-id]').each(function(){
   var pvKey = 'data-directory-rows-' + uuid;
   var config = Fliplet.Widget.getData(id);
 
-  if (config.source) {
-    if (!config.enable_live_data) {
-      return dataDirectory[id] = new DataDirectory(config, container);
-    }
+  /*
+   * Get data source data and format it to data directory format
+   */
+  function getData (options) {
+    options = options || { offline: true };
+    return Fliplet.DataSources.connect(config.source, options)
+      .then(function (source) {
+        return source.find();
+      })
+      .then(function formatData(rows) {
+        var formattedRows = rows.map(function (row) {
+          row.data.dataSourceEntryId = row.id;
+          return row.data;
+        });
 
-    Fliplet.Storage.get(pvKey)
-      .then(function (rows) {
-        if (rows) {
-          config.rows = rows;
-          dataDirectory[id] = new DataDirectory(config, container);
-        } else {
-          Fliplet.Storage.set(pvKey, config.rows);
-          dataDirectory[id] = new DataDirectory(config, container);
-        }
-
-        if (Fliplet.Navigator.isOnline()) {
-          Fliplet.DataSources.connect(config.source)
-            .then(function (source) {
-              return source.find();
-            })
-            .then(function (rows) {
-              dataDirectory[id].data = rows.map(function (row) {
-                row.data.dataSourceEntryId = row.id;
-                return row.data;
-              });
-
-              Fliplet.Storage.set(pvKey, dataDirectory[id].data);
-              dataDirectory[id].init();
-            });
-        }
+        return Promise.resolve(formattedRows);
+      })
+      .catch(function(error) {
+        console.log(error);
       });
   }
+
+  if (!config.source) {
+    return dataDirectory[id] = new DataDirectory(config, container);
+  }
+  
+  if (!config.enable_live_data || Fliplet.Env.get('platform') === 'web') {
+    return getData({ offline: true })
+      .then(function (data) {
+        config.rows = data;
+        return dataDirectory[id] = new DataDirectory(config, container);
+      });
+  }
+
+  Fliplet.Storage.get(pvKey)
+    .then(function (rows) {
+      if (rows) {
+        Fliplet.Storage.set(pvKey, config.rows);
+        config.rows = rows;
+        dataDirectory[id] = new DataDirectory(config, container);
+      }
+
+      if (Fliplet.Navigator.isOnline()) {
+        return getData({ offline: false })
+          .then(function (data) {
+            config.rows = data;
+            Fliplet.Storage.set(pvKey, data);
+            return dataDirectory[id] = new DataDirectory(config, container);
+          });
+      }
+
+      return Promise.reject();
+    })
+    .catch(function () {
+      // Start data directory with no data
+      dataDirectory[id] = new DataDirectory(config, container);
+    });
 });

c6c8b2771ca2eb7aecaa0c2bac25a9eb6e60c654
Joao-pina-fernandes <Joao.Pina.Fernandes@gmail.com>
2017-05-26 11:44:29 +0100

    auth token avaiable when we have an api endpoint

--- js/build-directory.js
+++ js/build-directory.js
@@ -491,6 +491,12 @@ DataDirectory.prototype.attachObservers = function(){
 
   _this.data.forEach(function(entry, i) {
     var imgURL = entry[_this.config.thumbnail_field];
+
+    if (imgURL.indexOf('api.fliplet.com') !== -1) {
+      // attach auth token
+      imgURL += (imgURL.indexOf('?') === -1 ? '?' : '&') + 'auth_token=' + Fliplet.User.getAuthToken();
+    }
+
     if ( /^(f|ht)tps?:\/\//i.test(imgURL) ) {
 
       var img = new Image();

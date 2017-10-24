e2414a93eb1438e16e86ab27952fc328684ea248
hcarneiro <hcarneiro@weboo.biz>
2017-10-24 15:50:00 +0100

    Clean up

--- js/build-directory.js
+++ js/build-directory.js
@@ -239,6 +239,11 @@ DataDirectory.prototype.initialiseHandlebars = function() {
 
   Handlebars.registerHelper('entry_thumbnail', function(entry) {
     var thumbnail = entry[_this.config.thumbnail_field];
+
+    if (/api\.fliplet\.(com|local)/.test(thumbnail)) {
+      // attach auth token
+      thumbnail += (thumbnail.indexOf('?') === -1 ? '?' : '&') + 'auth_token=' + Fliplet.User.getAuthToken();
+    }
     
     if (!thumbnail) {
       return '';
@@ -529,28 +534,6 @@ DataDirectory.prototype.isMode = function(mode) {
 
 DataDirectory.prototype.attachObservers = function() {
   var _this = this;
-
-  if (_this.data) {
-    _this.data.forEach(function(entry, i) {
-      var imgURL = entry[_this.config.thumbnail_field];
-
-      if (/api\.fliplet\.(com|local)/.test(imgURL)) {
-        // attach auth token
-        imgURL += (imgURL.indexOf('?') === -1 ? '?' : '&') + 'auth_token=' + Fliplet.User.getAuthToken();
-      }
-
-      if (/^(f|ht)tps?:\/\//i.test(imgURL)) {
-
-        var img = new Image();
-
-        img.addEventListener('load', function() {
-          $('.list-default.directory-entries li[data-index="' + i + '"] .list-image').css('background-image', 'url(' + this.src + ')');
-        }, false);
-
-        img.src = imgURL;
-      }
-    });
-  }
 
   this.$container.on('click', '.data-linked', $.proxy(this.dataLinkClicked, this));
   $(window).on('resize', function() {
@@ -1142,20 +1125,6 @@ DataDirectory.prototype.renderSearchResult = function(options, callback) {
 
       break;
   }
-
-  data.result.forEach(function(entry, i) {
-    var imgURL = entry[_this.config.thumbnail_field];
-    if (/^(f|ht)tps?:\/\//i.test(imgURL)) {
-
-      var img = new Image();
-
-      img.addEventListener('load', function() {
-        $('.list-default.search-result li[data-index="' + i + '"] .list-image').css('background-image', 'url(' + this.src + ')');
-      }, false);
-
-      img.src = imgURL;
-    }
-  });
 
   this.searchResultData = data.result;
   var searchResultTemplate = (this.config.searchResultTemplate !== '') ?

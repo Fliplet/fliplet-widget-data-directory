e5ee850366dd2741f23953e017213fcdbbcfb318
hcarneiro <hcarneiro@weboo.biz>
2018-02-14 16:50:44 +0000

    Another fix for moment helper

--- js/build-directory.js
+++ js/build-directory.js
@@ -166,7 +166,7 @@ DataDirectory.prototype.initialiseHandlebars = function() {
   });
 
   Handlebars.registerHelper('moment', function(key, format, obj) {
-    return moment(obj.data.root[key]).format(format);
+    return moment(key).format(format);
   });
 
   Handlebars.registerHelper('alphabet_divider', function() {
